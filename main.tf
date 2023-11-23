96daa7b819e513cb2583794444cd4d868214c494
Minchi Chiu <minchilcs@gmail.com>
2023-11-23 21:37:51 +0800

    add terraform kind resource

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "~> 0.0.19"
+    }
+  }
+}
+
+provider "kind" {
+}
+
+resource "kind_cluster" "default" {
+  name           = "kind"
+  wait_for_ready = true
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+    node {
+      role = "worker"
+    }
+    node {
+      role = "worker"
+    }
+    node {
+      role = "worker"
+    }
+
+    networking {
+      pod_subnet     = "10.10.0.0/16"
+      service_subnet = "10.11.0.0/16"
+    }
+  }
+}
